f67a37ce87620c90589da344fcf97c286655fe1e
nullStatus <dll2051@gmail.com>
2015-07-12 13:35:09 +0800

    init app

---
+++ WeiJu-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiJu-OC
+//
+//  Created by light on 15/7/12.
+//  Copyright (c) 2015年 tuotuode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeiJu-OC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeiJu-OC
+//
+//  Created by light on 15/7/12.
+//  Copyright (c) 2015年 tuotuode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
